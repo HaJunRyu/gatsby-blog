33fc09b72f411f3b0682fafbda2ad25c87838a16
HaJunRyu <hjr4557@naver.com>
2022-07-29 02:31:05 +0900

    Feat: 재활용 될 레이아웃을 Template 컴포넌트로 분리한다

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import React, { FunctionComponent, useMemo } from 'react';
-import styled from '@emotion/styled';
-import GlobalStyle from 'components/Common/GlobalStyle';
-import Footer from 'components/Common/Footer';
 import CategoryList, { CategoryListProps } from 'components/Main/CategoryList';
 import Introduction from 'components/Main/Introduction';
 import PostList from 'components/Main/PostList';
@@ -9,6 +6,7 @@ import { graphql } from 'gatsby';
 import { PostListItemType } from 'types/PostItem.types';
 import { IGatsbyImageData } from 'gatsby-plugin-image';
 import queryString, { ParsedQuery } from 'query-string';
+import Template from 'components/Common/Template';
 
 type IndexPageProps = {
   location: {
@@ -25,12 +23,6 @@ type IndexPageProps = {
     };
   };
 };
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`;
 
 const IndexPage: FunctionComponent<IndexPageProps> = function ({
   location: { search },
@@ -70,13 +62,11 @@ const IndexPage: FunctionComponent<IndexPageProps> = function ({
     []
   );
   return (
-    <Container>
-      <GlobalStyle />
+    <Template>
       <Introduction profileImage={gatsbyImageData} />
       <CategoryList selectedCategory={selectedCategory} categoryList={categoryList} />
       <PostList selectedCategory={selectedCategory} posts={edges} />
-      <Footer />
-    </Container>
+    </Template>
   );
 };
 
